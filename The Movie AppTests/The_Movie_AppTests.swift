cdfe49d317442c1084de8d09624605b3f49165f8
Tolga Caner <caner.tolga@gmail.com>
2017-05-31 23:27:36 +0300

    initial

---
+++ The Movie AppTests/The_Movie_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Movie_AppTests.swift
+//  The Movie AppTests
+//
+//  Created by Tolga Caner on 31/05/2017.
+//  Copyright © 2017 Tolga Caner. All rights reserved.
+//
+
+import XCTest
+@testable import The_Movie_App
+
+class The_Movie_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
