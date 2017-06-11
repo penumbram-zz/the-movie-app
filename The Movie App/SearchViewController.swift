2e920abcd4e789ef78083446ea797e365d16b08b
Tolga Caner <caner.tolga@gmail.com>
2017-06-11 17:10:02 +0300

    Only portrait allowed

---
+++ The Movie App/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  The Movie App
+//
+//  Created by Tolga Caner on 09/06/2017.
+//  Copyright © 2017 Tolga Caner. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+}
